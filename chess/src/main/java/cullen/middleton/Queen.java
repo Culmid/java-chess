aff8dd3314ff6a653e86eabfdf358cf6fed2e07c
CDE Middleton <21785368@sun.ac.za>
2022-01-10 23:36:14 +0200

    Rook/Queen Movement

--- chess/src/main/java/cullen/middleton/Queen.java
+++ chess/src/main/java/cullen/middleton/Queen.java
@@ -6,8 +6,156 @@ public class Queen extends Piece {
         super(c, x, y);
     }
     
+    // TODO: Figure Out How to Share - Rook/Bishop
     public int[] legalMoves(Board brd) {
-        return null;
+        int[] lm = new int[20];
+
+        // Up-Right Diag
+        int potx = x + 1;
+        int poty = y + 1;
+        int count = 0;
+
+        while (potx < 8 && poty < 8) {
+            Piece p = brd.getPiece(potx, poty);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = potx++;
+            lm[count++] = poty++;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        // Bottom-Left Diag
+        potx = x - 1;
+        poty = y - 1;
+
+        while (potx > -1 && poty > -1) {
+            Piece p = brd.getPiece(potx, poty);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = potx--;
+            lm[count++] = poty--;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        // Top-Left Diag
+        potx = x - 1;
+        poty = y + 1;
+
+        while (potx > -1 && poty < 8) {
+            Piece p = brd.getPiece(potx, poty);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = potx--;
+            lm[count++] = poty++;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        // Bottom-Right Diag
+        potx = x + 1;
+        poty = y - 1;
+
+        while (potx < 8 && poty > -1) {
+            Piece p = brd.getPiece(potx, poty);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = potx++;
+            lm[count++] = poty--;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        // Up
+        poty = y + 1;
+        while (poty < 8) {
+            Piece p = brd.getPiece(x, poty);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = x;
+            lm[count++] = poty++;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        // Down
+        poty = y - 1;
+        while (poty > -1) {
+            Piece p = brd.getPiece(x, poty);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = x;
+            lm[count++] = poty--;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        // Right
+        potx = x - 1;
+        while (potx > -1) {
+            Piece p = brd.getPiece(potx, y);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = potx--;
+            lm[count++] = y;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        // Left
+        potx = x + 1;
+        while (potx < 8) {
+            Piece p = brd.getPiece(potx, y);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = potx++;
+            lm[count++] = y;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        return lm;
     }
 
     public String toString() {
--- chess/src/main/java/cullen/middleton/Rook.java
+++ chess/src/main/java/cullen/middleton/Rook.java
@@ -7,7 +7,78 @@ public class Rook extends Piece {
     }
     
     public int[] legalMoves(Board brd) {
-        return null;
+        int[] lm = new int[20];
+        int count = 0;
+
+        // Up
+        int poty = y + 1;
+        while (poty < 8) {
+            Piece p = brd.getPiece(x, poty);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = x;
+            lm[count++] = poty++;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        // Down
+        poty = y - 1;
+        while (poty > -1) {
+            Piece p = brd.getPiece(x, poty);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = x;
+            lm[count++] = poty--;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        // Right
+        int potx = x - 1;
+        while (potx > -1) {
+            Piece p = brd.getPiece(potx, y);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = potx--;
+            lm[count++] = y;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        // Left
+        potx = x + 1;
+        while (potx < 8) {
+            Piece p = brd.getPiece(potx, y);
+
+            if (p != null && p.getC() == c) {
+                break;
+            }
+
+            lm[count++] = potx++;
+            lm[count++] = y;
+
+            if (p != null) {
+                break;
+            }
+        }
+
+        return lm;
     }
 
     public String toString() {
